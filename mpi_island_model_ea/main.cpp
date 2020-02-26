9fa253c8b98e914421c12d47365ce81b20bf0989
Bradley Morgan <bradley@eightbitapps.com>
2020-02-26 08:10:10 -0600

    Initial Commit

---
+++ mpi_island_model_ea/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  mpi_island_model_ea
+//
+//  Created by Bradley Morgan on 2/26/20.
+//  Copyright © 2020 Bradley Morgan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
