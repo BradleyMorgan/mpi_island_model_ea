3295a0d23ebbe5173e3160f96a8ce97b1fcb6fa7
Bradley Morgan <bradley@eightbitapps.com>
2020-02-27 14:53:12 -0600

    fixed typo, extraneous characters in config

--- mpi_island_model_ea/config.h
+++ mpi_island_model_ea/config.h
@@ -13,8 +13,7 @@ const static unsigned int EVALS = 20000;
 const static unsigned int RUNS = 100;
 const static unsigned int LAMBDA = 200;
 const static unsigned int MU = 2000;
-const static unsigned int DIM = 5sort
-0;
+const static unsigned int DIM = 50;
 const static double MUTATION_RATE = 0.05;
 
 #endif /* config_h */
