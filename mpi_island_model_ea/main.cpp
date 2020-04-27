1a2046fa671b8aa7f8947d5d22f824773e867057
Bradley Morgan <bradley@eightbitapps.com>
2020-04-26 20:40:36 -0500

    new method for topology distribution

--- mpi_island_model_ea/main.cpp
+++ mpi_island_model_ea/main.cpp
@@ -165,15 +165,6 @@ int main(int argc, const char * argv[]) {
         MPI_Comm tcomm;
         MPI_Comm_dup(MPI_COMM_WORLD, &tcomm);
         
-//        int procnum, proclen;
-//        char procname[MPI_MAX_PROCESSOR_NAME];
-//
-//        MPI_Get_processor_name(procname, &proclen);
-//
-//        GETCPU(procnum);
-//
-//        LOG(6, 0, 0, "** Process %d in world size %d on core %d processor %s\r\n", world_rank, world_size, procnum, procname);
-        
         // separate the single full population from the root process to subpopulations across all processes ...
         
         LOG(10, world_rank, 0, "scattering population ...\r\n");
@@ -194,23 +185,6 @@ int main(int argc, const char * argv[]) {
             if(world_rank == 0) {
         
                 LOG(10, world_rank, 0, "T = (%d)mod(%d) = %d\r\n", eval-1, config::topo_mu, t);
-                
-                for(int i=0; i<world_size; i++) {
-                    
-                    send_size = (int)topologies[t].comm[i].senders.size();
-                    rec_size = (int)topologies[t].comm[i].receivers.size();
-                    
-                    LOG(8, 0, 0, "sending topology %d (senders = %d, receivers = %d) to %d ...\r\n", t, send_size, rec_size, i);
-                    
-                    LOG(10, 0, 0, "sending %d senders to rank %d ...\r\n", send_size, i);
-                    MPI_Send(&send_size, 1, MPI_INT, i, 1, tcomm);
-                    MPI_Send(&topologies[t].comm[i].senders[0], send_size, MPI_INT, i, 2, tcomm);
-                    
-                    LOG(10, 0, 0, "sending %d receivers to rank %d ...\r\n", rec_size, i);
-                    MPI_Send(&rec_size, 1, MPI_INT, i, 3, tcomm);
-                    MPI_Send(&topologies[t].comm[i].receivers[0], rec_size, MPI_INT, i, 4, tcomm);
-                    
-                }
                 
                 if(eval != 1 && t == 0) {
                 
@@ -243,29 +217,28 @@ int main(int argc, const char * argv[]) {
                         
                     }
                     
+                } else {
+                    
+                    for(int i=0; i<world_size; i++) {
+                        
+                        send_size = (int)topologies[t].comm[i].senders.size();
+                        rec_size = (int)topologies[t].comm[i].receivers.size();
+                        
+                        LOG(8, 0, 0, "sending topology %d (senders = %d, receivers = %d) to %d ...\r\n", t, send_size, rec_size, i);
+                        
+                        LOG(10, 0, 0, "sending %d senders to rank %d ...\r\n", send_size, i);
+                        MPI_Send(&send_size, 1, MPI_INT, i, 1, tcomm);
+                        MPI_Send(&topologies[t].comm[i].senders[0], send_size, MPI_INT, i, 2, tcomm);
+                        
+                        LOG(10, 0, 0, "sending %d receivers to rank %d ...\r\n", rec_size, i);
+                        MPI_Send(&rec_size, 1, MPI_INT, i, 3, tcomm);
+                        MPI_Send(&topologies[t].comm[i].receivers[0], rec_size, MPI_INT, i, 4, tcomm);
+                        
+                    }
+                    
                 }
                 
             }
-            
-            MPI_Recv(&send_size, 1, MPI_INT, 0, 1, tcomm, MPI_STATUS_IGNORE);
-            isle.senders.clear();
-            isle.senders.resize(send_size);
-            
-            MPI_Recv(&isle.senders[0], send_size, MPI_INT, 0, 2, tcomm, MPI_STATUS_IGNORE);
-            LOG(10, 0, 0, "rank %d received %lu senders: ", world_rank, isle.senders.size());
-            for(int i=0; i<isle.senders.size(); i++) { LOG(10, 0, 0, "%d ", isle.senders[i]); }
-            LOG(10, 0, 0, "\r\n");
-            
-            MPI_Recv(&rec_size, 1, MPI_INT, 0, 3, tcomm, MPI_STATUS_IGNORE);
-            isle.receivers.clear();
-            isle.receivers.resize(rec_size);
-            
-            MPI_Recv(&isle.receivers[0], rec_size, MPI_INT, 0, 4, tcomm, MPI_STATUS_IGNORE);
-            LOG(10, 0, 0, "rank %d got %lu receivers: ", world_rank, isle.receivers.size());
-            for(int i=0; i<isle.receivers.size(); i++) { LOG(10, 0, 0, "%d ", isle.receivers[i]); }
-            LOG(10, 0, 0, "\r\n");
-            
-            MPI_Barrier(tcomm);
             
             if(eval != 1 && t == 0) {
                 
@@ -292,6 +265,26 @@ int main(int argc, const char * argv[]) {
                 LOG(8, 0, 0, "rank %d got topology (senders = %lu, receivers = %lu)\r\n", world_rank, isle.senders.size(), isle.receivers.size());
                 
                 MPI_Barrier(tcomm);
+                
+            } else {
+                
+                MPI_Recv(&send_size, 1, MPI_INT, 0, 1, tcomm, MPI_STATUS_IGNORE);
+                isle.senders.clear();
+                isle.senders.resize(send_size);
+                
+                MPI_Recv(&isle.senders[0], send_size, MPI_INT, 0, 2, tcomm, MPI_STATUS_IGNORE);
+                LOG(10, 0, 0, "rank %d received %lu senders: ", world_rank, isle.senders.size());
+                for(int i=0; i<isle.senders.size(); i++) { LOG(10, 0, 0, "%d ", isle.senders[i]); }
+                LOG(10, 0, 0, "\r\n");
+                
+                MPI_Recv(&rec_size, 1, MPI_INT, 0, 3, tcomm, MPI_STATUS_IGNORE);
+                isle.receivers.clear();
+                isle.receivers.resize(rec_size);
+                
+                MPI_Recv(&isle.receivers[0], rec_size, MPI_INT, 0, 4, tcomm, MPI_STATUS_IGNORE);
+                LOG(10, 0, 0, "rank %d got %lu receivers: ", world_rank, isle.receivers.size());
+                for(int i=0; i<isle.receivers.size(); i++) { LOG(10, 0, 0, "%d ", isle.receivers[i]); }
+                LOG(10, 0, 0, "\r\n");
                 
             }
             
