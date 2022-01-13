826245eb531624c4eff237a092e7f4feb43652e4
Bradley Morgan <bradley@eightbitapps.com>
2022-01-13 11:57:32 -0600

    changed abs to fabs for distance calc

--- mpi_island_model_ea/dtype_meta.h
+++ mpi_island_model_ea/dtype_meta.h
@@ -224,7 +224,7 @@ double calculate_distance_o1(std::vector<topology*> &subjects, unsigned idx) {
     
     LOG(2, 0, 0, "calculating front index %d o1 distance with abs(%f - %f) / (%f - %f)\r\n", idx, subjects[idx+1]->fitness_multi.first, subjects[idx-1]->fitness_multi.first, subjects[subjects.size()-1]->fitness_multi.first, subjects[0]->fitness_multi.first);
     
-    double result = subjects[idx]->distance + abs(subjects[idx+1]->fitness_multi.first - subjects[idx-1]->fitness_multi.first) / abs(subjects[subjects.size()-1]->fitness_multi.first - subjects[0]->fitness_multi.first);
+    double result = subjects[idx]->distance + fabs(subjects[idx+1]->fitness_multi.first - subjects[idx-1]->fitness_multi.first) / fabs(subjects[subjects.size()-1]->fitness_multi.first - subjects[0]->fitness_multi.first);
     
     LOG(2, 0, 0, "calculated front index %d o1 distance = %f\r\n", idx, result);
     
@@ -236,7 +236,7 @@ double calculate_distance_o2(std::vector<topology*> &subjects, unsigned idx) {
     
     LOG(2, 0, 0, "calculating front index %d o1 distance with abs(%f - %f) / (%f - %f)\r\n", idx, subjects[idx+1]->fitness_multi.second, subjects[idx-1]->fitness_multi.second, subjects[subjects.size()-1]->fitness_multi.second, subjects[0]->fitness_multi.second);
     
-    double result = subjects[idx]->distance + abs(subjects[idx+1]->fitness_multi.second - subjects[idx-1]->fitness_multi.second) / abs(subjects[subjects.size()-1]->fitness_multi.second - subjects[0]->fitness_multi.second);;
+    double result = subjects[idx]->distance + fabs(subjects[idx+1]->fitness_multi.second - subjects[idx-1]->fitness_multi.second) / fabs(subjects[subjects.size()-1]->fitness_multi.second - subjects[0]->fitness_multi.second);;
     
     LOG(2, 0, 0, "calculated front index %d o2 distance = %f\r\n", idx, result);
     
--- mpi_island_model_ea/scripts/build.sh
+++ mpi_island_model_ea/scripts/build.sh
@@ -5,4 +5,6 @@ cp config.txt config.old
 git stash
 git pull
 mpic++ -g -O3 -std=c++11 main.cpp -o ./island
+cp config.txt config.pull
+cp config.old config.txt
 
