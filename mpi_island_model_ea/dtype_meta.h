4c6b53d943b44f80bb803511e58b28c27343a15f
Bradley Morgan <bradley@eightbitapps.com>
2022-01-13 09:14:20 -0600

    getting unexpected distance calculation results, added more logging

--- mpi_island_model_ea/dtype_meta.h
+++ mpi_island_model_ea/dtype_meta.h
@@ -222,9 +222,11 @@ void plot_fronts(std::vector<std::vector<topology*>> &fronts) {
 
 double calculate_distance_o1(std::vector<topology*> &subjects, unsigned idx) {
     
+    LOG(2, 0, 0, "calculating front index %d o1 distance with [%f][%f][%f]\r\n", idx, subjects[idx+1]->fitness_multi.first, subjects[idx]->fitness_multi.first, subjects[idx-1]->fitness_multi.first);
+    
     double result = subjects[idx]->distance + abs(subjects[idx+1]->fitness_multi.first - subjects[idx-1]->fitness_multi.first) / abs(subjects[subjects.size()-1]->fitness_multi.first - subjects[0]->fitness_multi.first);
     
-    LOG(2, 0, 0, "front index %d o1 distance = %f\r\n", idx, result);
+    LOG(2, 0, 0, "calculated front index %d o1 distance = %f\r\n", idx, result);
     
     return result;
     
@@ -232,9 +234,11 @@ double calculate_distance_o1(std::vector<topology*> &subjects, unsigned idx) {
         
 double calculate_distance_o2(std::vector<topology*> &subjects, unsigned idx) {
     
+    LOG(2, 0, 0, "calculating front index %d o2 distance with [%f][%f][%f]\r\n", idx, subjects[idx+1]->fitness_multi.first, subjects[idx]->fitness_multi.first, subjects[idx-1]->fitness_multi.first);
+    
     double result = subjects[idx]->distance + abs(subjects[idx+1]->fitness_multi.second - subjects[idx-1]->fitness_multi.second) / abs(subjects[subjects.size()-1]->fitness_multi.second - subjects[0]->fitness_multi.second);;
     
-    LOG(2, 0, 0, "front index %d o2 distance = %f\r\n", idx, result);
+    LOG(2, 0, 0, "calculated front index %d o2 distance = %f\r\n", idx, result);
     
     return result;
     
